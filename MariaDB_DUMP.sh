2a4a9cd1bd70686ca40c9032be1d74a23cce2433
Stephan <synotr@geimist.eu>
2023-02-13 16:37:08 +0000

    „MariaDB_DUMP.sh“ hinzufügen

---
+++ MariaDB_DUMP.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+# MARIADB-BackUp
+# https://stefankonarski.de/content/mysql-backup-fuer-jede-datenbank-eine-datei
+# /volume1/homes/admin/script/MariaDB_DUMP.sh
+
+# MySQL-Rootkennwort:
+    MYSQLPW="GanzSicherMariaDB_PW"
+
+# Sicherungsverzeichenis:
+    BACKUPDIR="/volume1/system/DS_BackUps/MariaDB_DUMP"
+
+# Datumsformat:
+    DATE=$(date +%Y-%m-%d_%H-%M-%S)
+    PREFIX="MySQLdump_$(date +'%Y-%m-%d_%H-%M-%S')_"
+    DBengine=/volume*/@appstore/MariaDB10/usr/local/mariadb10/bin/mysql
+
+#   mysqldump="/volume1/@appstore/MariaDB10/usr/local/mariadb10/bin/mysqldump"
+    mysqldump="/usr/local/mariadb10/bin/mysqldump"
+
+# Exclude the following databases
+    ExDB="phpmyadmin information_schema performance_schema"
+
+
+# -----------------------------------------------------
+
+
+if [ "" = "$MYSQLPW" ]; then
+    echo "Login ohne Passwort"
+    DBLOGIN='-u root'
+else
+    DBLOGIN="-u root -p$MYSQLPW"
+fi
+
+
+DBlist="$($DBengine $DBLOGIN -Bse 'show databases')"
+# List for compressing files
+FILE2COMPRESS=
+DBCOUNT=0
+SKIPDBCOUNT=0
+
+# Loop over all databases
+for db in $DBlist ; do
+    # Don't skip any database as default
+    skipdb=0
+    # If excludable databases are defined
+    if [ "$ExDB" != "" ]; then
+        # Loop over excludable databases
+        for n in $ExDB; do
+            if [ "$db" = "$n" ]; then
+                skipdb=1
+                break;
+            fi
+        done
+    fi
+ 
+    if [ "$skipdb" = "1" ] ; then
+        echo "Skip database $db"
+        SKIPDBCOUNT=$(($SKIPDBCOUNT + 1))
+        continue
+    fi
+ 
+    DBCOUNT=$(($DBCOUNT + 1))
+    
+    fn="${BACKUPDIR}/${prefix}${db}.sql"
+    echo "Dump database $db to $fn"
+    
+    $mysqldump $DBLOGIN --databases $db | gzip -c -9 > "${BACKUPDIR}/MySQLdump_${db}_${DATE}.sql.gz"
+
+# Rotation, sofern man das Skript "archive_rotate" von hier verwendet: https://git.geimist.eu/geimist/archive_rotate
+#   /volume1/Pfad_zu/archive_rotate.sh -vc -p="${BACKUPDIR}" -s=MySQLdump_${db}* -h=1x4 -d=24x7 -w=7x4 -m=4x6 -y=4x*
+done
+
+echo -e
+echo "    saved DB's:     $DBCOUNT"
+echo "    skiped DB's:    $SKIPDBCOUNT"
+
+    $mysqldump --opt $DBLOGIN --all-databases | gzip -c -9 > ${BACKUPDIR}/MySQLdump_GESAMTBACKUP_${DATE}.gz
+# Rotation, sofern man das Skript "archive_rotate" von hier verwendet: https://git.geimist.eu/geimist/archive_rotate
+#   /volume1/Pfad_zu/archive_rotate.sh -vc -p="${BACKUPDIR}" -s=MySQLdump_GESAMTBACKUP_* -h=1x4 -d=24x7 -w=7x4 -m=4x6 -y=4x*
+
+
+exit 0
