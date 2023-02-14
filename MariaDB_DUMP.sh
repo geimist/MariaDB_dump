f28d867f59c59b050b348055e915230eb9efa2b3
Stephan <synotr@geimist.eu>
2023-02-14 20:19:27 +0000

    „MariaDB_DUMP.sh“ ändern

--- MariaDB_DUMP.sh
+++ MariaDB_DUMP.sh
@@ -2,21 +2,30 @@
 #####################################################################################
 #                                                                                   #
 #   MARIADB-BackUp                                                                  #
-#   v1.1.0 @ 2023-02-13                                                             #
+#   v1.1.1 @ 2023-02-14                                                             #
 #                                                                                   #
 #   source:                                                                         #
 #   https://stefankonarski.de/content/mysql-backup-fuer-jede-datenbank-eine-datei   #
 #                                                                                   #
 #####################################################################################
 
+# changelog v1.1.1:
+#   - 1 Sekunde Wartepause zwichen jedem Backup eingebaut, damit das Rotationsskript korret läuft
+#   - Schalter für ein Gesamtbackup eingebaut
+
+# -----------------------------------------------------
+
 # MySQL-Rootkennwort:
     MYSQLPW="GanzSicherMariaDB_PW"
 
 # Sicherungsverzeichenis:
-    BACKUPDIR="/volume1/system/DS_BackUps/MariaDB_DUMP"
+    BACKUPDIR="/volume1/system/DS_BackUps/MariaDB_DUMP_test"
 
 # ein Unterverzeichnis für jede DB nutzen? (true = ja / alles andere = nein)
     useSubDir=true
+
+# auch ein Gesamtbackup anlegen? (true = ja / alles andere = nein)
+    DumpAll=true
 
 # Datumsformat für Dateiname:
     DATE=$(date +%Y-%m-%d_%H-%M-%S)
@@ -27,7 +36,6 @@
 
 # auszuschließende Datenbanken:
     ExDB="phpmyadmin information_schema performance_schema"
-
 
 # -----------------------------------------------------
 
@@ -80,31 +88,36 @@ for db in $DBlist ; do
 
     fn="${modBACKUPDIR}/MySQLdump_${db}_${DATE}.sql.gz"
 
-    echo "Dump Datenbank $db nach ${fn}"
+    printf "\n\nDump Datenbank $db nach ${fn}\n\n"
     
     $mysqldump $DBLOGIN --databases $db | gzip -c -9 > "${fn}"
+    sleep 1
 
 # Rotation, sofern man das Skript "archive_rotate" von hier verwendet: https://git.geimist.eu/geimist/archive_rotate
-#   /volume1/Pfad_zu/archive_rotate.sh -vc -p="${modBACKUPDIR}" -s=MySQLdump_${db}* -h=1x4 -d=24x7 -w=7x4 -m=4x6 -y=4x*
+#    /volume1/homes/admin/script/_funktionen/archive_rotate.sh -vc -p="${modBACKUPDIR}" -s=MySQLdump_${db}* -h=1x4 -d=24x7 -w=7x4 -m=4x6 -y=4x*
 done
+
+# Und zum Schluss noch ein Gesamtbackup:
+    if [ "$DumpAll" = true ]; then
+        printf "\n\nDump GESAMT $db nach ${fn}\n\n"
+        DBCOUNT=$(($DBCOUNT + 1))
+        modBACKUPDIR="${BACKUPDIR}"
+        if [ "$useSubDir" = true ]; then
+            modBACKUPDIR="${BACKUPDIR}/GESAMT"
+            if [ ! -d "${modBACKUPDIR}" ]; then
+                mkdir -p "${modBACKUPDIR}"
+            fi
+        fi
+
+        $mysqldump --opt $DBLOGIN --all-databases | gzip -c -9 > ${modBACKUPDIR}/MySQLdump_GESAMTBACKUP_${DATE}.gz
+        sleep 1
+
+    # Rotation, sofern man das Skript "archive_rotate" von hier verwendet: https://git.geimist.eu/geimist/archive_rotate
+#        /volume1/homes/admin/script/_funktionen/archive_rotate.sh -vc -p="${modBACKUPDIR}" -s=MySQLdump_GESAMTBACKUP_* -h=1x4 -d=24x7 -w=7x4 -m=4x6 -y=4x*
+    fi
 
 echo -e
 echo "    gesicherte DB's:      $DBCOUNT"
 echo "    übersprungene DB's:   $SKIPDBCOUNT"
-
-# Und zum Schluss noch ein Gesamtbackup:
-    modBACKUPDIR="${BACKUPDIR}"
-    if [ "$useSubDir" = true ]; then
-        modBACKUPDIR="${BACKUPDIR}/GESAMT"
-        if [ ! -d "${modBACKUPDIR}" ]; then
-            mkdir -p "${modBACKUPDIR}"
-        fi
-    fi
-
-    $mysqldump --opt $DBLOGIN --all-databases | gzip -c -9 > ${modBACKUPDIR}/MySQLdump_GESAMTBACKUP_${DATE}.gz
-
-# Rotation, sofern man das Skript "archive_rotate" von hier verwendet: https://git.geimist.eu/geimist/archive_rotate
-#   /volume1/Pfad_zu/archive_rotate.sh -vc -p="${modBACKUPDIR}" -s=MySQLdump_GESAMTBACKUP_* -h=1x4 -d=24x7 -w=7x4 -m=4x6 -y=4x*
-
 
 exit 0
