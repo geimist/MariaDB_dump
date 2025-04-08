f7357197be133393de0d1f3ea7bb01020ff00dff
luddinho <info@luddi.org>
2025-04-08 08:09:28 +0200

    reworked script and fixed some issues

--- MariaDB_DUMP.sh
+++ MariaDB_DUMP.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# shellcheck disable=SC2154
+
 #####################################################################################
 #                                                                                   #
 #   MARIADB-BackUp                                                                  #
@@ -15,27 +17,23 @@
 
 # -----------------------------------------------------
 
-# MySQL-Rootkennwort:
-    MYSQLPW="GanzSicherMariaDB_PW"
+# Variablen
+# Umgebungsvariablen
 
-# Sicherungsverzeichenis:
-    BACKUPDIR="/volume1/system/DS_BackUps/MariaDB_DUMP_test"
+# Konfigurationsdatei:
+ENV_CONFIG="$1"
+if [ -z "$ENV_CONFIG" ]; then
+    ENV_CONFIG="./cnf/config.cnf"
+fi
 
-# ein Unterverzeichnis für jede DB nutzen? (true = ja / alles andere = nein)
-    useSubDir=true
+if [ ! -f "$ENV_CONFIG" ]; then
+    echo "Konfigurationsdatei $ENV_CONFIG nicht gefunden!"
+    exit 1
+else
+    # shellcheck disable=SC1090
+    source "$ENV_CONFIG"
+fi
 
-# auch ein Gesamtbackup anlegen? (true = ja / alles andere = nein)
-    DumpAll=true
-
-# Datumsformat für Dateiname:
-    DATE=$(date +%Y-%m-%d_%H-%M-%S)
-
-# Programmpfade:
-    DBengine="/var/packages/MariaDB10/target/usr/local/mariadb10/bin/mysql"
-    mysqldump="/usr/local/mariadb10/bin/mysqldump"
-
-# auszuschließende Datenbanken:
-    ExDB="phpmyadmin information_schema performance_schema"
 
 # -----------------------------------------------------
 
@@ -50,6 +48,7 @@ else
     DBLOGIN="-u root -p$MYSQLPW"
 fi
 
+# shellcheck disable=SC2086
 DBlist="$($DBengine $DBLOGIN -Bse 'show databases')"
 
 DBCOUNT=0
@@ -69,52 +68,80 @@ for db in $DBlist ; do
             fi
         done
     fi
- 
+
     if [ "$skipdb" = "1" ] ; then
         echo "überspringe Datenbank $db"
-        SKIPDBCOUNT=$(($SKIPDBCOUNT + 1))
+        SKIPDBCOUNT=$((SKIPDBCOUNT + 1))
         continue
     fi
- 
-    DBCOUNT=$(($DBCOUNT + 1))
+
+    DBCOUNT=$((DBCOUNT + 1))
 
     modBACKUPDIR="${BACKUPDIR}"
     if [ "$useSubDir" = true ]; then
         modBACKUPDIR="${BACKUPDIR}/${db}"
         if [ ! -d "${modBACKUPDIR}" ]; then
             mkdir -p "${modBACKUPDIR}"
-        fi        
+        fi
     fi
 
     fn="${modBACKUPDIR}/MySQLdump_${db}_${DATE}.sql.gz"
 
-    printf "\n\nDump Datenbank $db nach ${fn}\n\n"
-    
+    printf "\n\nDump Datenbank $db nach %s\n\n" "${fn}"
+
+    # shellcheck disable=SC2086
+    # shellcheck disable=SC2154
     $mysqldump $DBLOGIN --databases $db | gzip -c -9 > "${fn}"
+    retcode=$?
+    if [ $retcode -ne 0 ]; then
+        echo "Fehler beim Erzeugen eines Dumps der Datenbank $db"
+        exit $retcode
+    else
+        echo "Dump der Datenbank $db erfolgreich"
+        DBCOUNT=$((DBCOUNT + 1))
+    fi
     sleep 1
 
-# Rotation, sofern man das Skript "archive_rotate" von hier verwendet: https://git.geimist.eu/geimist/archive_rotate
-#    /volume1/Pfad_zu/archive_rotate.sh -vc -p="${modBACKUPDIR}" -s=MySQLdump_${db}* -h=1x4 -d=24x7 -w=7x4 -m=4x6 -y=4x*
+    # Rotation, sofern man das Skript "archive_rotate" von hier verwendet: https://github.com/geimist/archive_rotate
+    if [ "$Rotate" = true ]; then
+        echo "Start Rotation of Dumps..."
+        # shellcheck disable=SC2154
+        "${ScriptRotate}" -vc -p="${modBACKUPDIR}" -s=MySQLdump_"${db}"_* -h=1x4 -d=24x7 -w=7x4 -m=4x6 -y=4x*
+    fi
 done
 
 # Und zum Schluss noch ein Gesamtbackup:
-    if [ "$DumpAll" = true ]; then
-        printf "\n\nDump GESAMT $db nach ${fn}\n\n"
-        DBCOUNT=$(($DBCOUNT + 1))
-        modBACKUPDIR="${BACKUPDIR}"
-        if [ "$useSubDir" = true ]; then
-            modBACKUPDIR="${BACKUPDIR}/GESAMT"
-            if [ ! -d "${modBACKUPDIR}" ]; then
-                mkdir -p "${modBACKUPDIR}"
-            fi
+if [ "$DumpAll" = true ]; then
+    modBACKUPDIR="${BACKUPDIR}"
+    # Wenn ein Unterverzeichnis für die DBs genutzt werden soll, dann einen Unterordner für das Gesamtbackup anlegen und modBACKUPDIR anpassen:
+    if [ "$useSubDir" = true ]; then
+        modBACKUPDIR="${BACKUPDIR}/GESAMT"
+        if [ ! -d "${modBACKUPDIR}" ]; then
+            mkdir -p "${modBACKUPDIR}"
         fi
-
-        $mysqldump --opt $DBLOGIN --all-databases | gzip -c -9 > ${modBACKUPDIR}/MySQLdump_GESAMTBACKUP_${DATE}.gz
-        sleep 1
-
-    # Rotation, sofern man das Skript "archive_rotate" von hier verwendet: https://git.geimist.eu/geimist/archive_rotate
-#        /volume1/Pfad_zu/archive_rotate.sh -vc -p="${modBACKUPDIR}" -s=MySQLdump_GESAMTBACKUP_* -h=1x4 -d=24x7 -w=7x4 -m=4x6 -y=4x*
     fi
+    fn="${modBACKUPDIR}/MySQLdump_GESAMTBACKUP_${DATE}.sql.gz"
+    printf "\n\nDump GESAMT nach %s\n\n" "${fn}"
+
+    # shellcheck disable=SC2086
+    $mysqldump --opt $DBLOGIN --all-databases | gzip -c -9 > "${fn}"
+    retcode=$?
+    if [ $retcode -ne 0 ]; then
+        echo "Fehler beim Erzeugen eines Dumps der GESAMT-Datenbank"
+        exit $retcode
+    else
+        echo "Dump der GESAMT-Datenbank erfolgreich"
+        DBCOUNT=$((DBCOUNT + 1))
+    fi
+    sleep 1
+
+
+    # Rotation, sofern man das Skript "archive_rotate" von hier verwendet: https://github.com/geimist/archive_rotate
+    if [ "$Rotate" = true ]; then
+        echo "Start Rotation of Dumps..."
+        "${ScriptRotate}" -vc -p="${modBACKUPDIR}" -s=MySQLdump_GESAMTBACKUP_* -h=1x4 -d=24x7 -w=7x4 -m=4x6 -y=4x*
+    fi
+fi
 
 echo -e
 echo "    gesicherte DB's:      $DBCOUNT"
